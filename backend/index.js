8d1d2fcfda2d5f37cbe7cc15928fd15a0a650cd1
hindavilande05 <landehindavi05@gmail.com>
2023-04-03 16:14:23 +0530

    data stored

--- backend/index.js
+++ backend/index.js
@@ -6,9 +6,24 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use(express.json())
+
+//Available Routes
+
+app.use('/api/auth', require('./routes/auth'))
+app.use('./api/notes', require('./routes/notes'))
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
+
+// app.get('/api/v1/login', (req,res) => {
+//     res.send('Hello login!')
+// })
+
+// app.get('/api/v1/signup', (req,res) => {
+//     res.send('Hello signup!')
+// })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
